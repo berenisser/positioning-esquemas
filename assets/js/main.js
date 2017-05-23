92807d2fc58b21b0e7f8c72bd9de5bbbcfbf22bb
berenisser <berenisser@gmail.com>
2017-05-22 23:52:02 -0400

    Blue box created

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,4 +35,40 @@ function crearCajaRoja(){
 	contenedor.appendChild(cajaRoja);
 }
 
+function crearCajaAzul(){
+	var contenedor = document.getElementById("contenedor");
+	contenedor.style.position = "relative";
+	
+	//Creo la caja Azul y le doy estilos
+	var cajaAzul = document.createElement("div");
+	cajaAzul.style.position = "absolute";
+	cajaAzul.style.top = "170px";
+	cajaAzul.style.background = "blue";
+	cajaAzul.style.width = "160px";
+	cajaAzul.style.height = "160px";
+
+	//Creo caja Verde y le doy estilos
+	var cajaVerde = document.createElement("div");
+	cajaVerde.style.position = "absolute";
+	cajaVerde.style.left = "40px";
+	cajaVerde.style.top = "40px";
+	cajaVerde.style.background = "green";
+	cajaVerde.style.width = "80px";
+	cajaVerde.style.height = "80px";
+
+	//Creo caja Negra y le doy estilos
+	var cajaNegra = document.createElement("div");
+	cajaNegra.style.position = "absolute";
+	cajaNegra.style.left = "0";
+	cajaNegra.style.top = "0";
+	cajaNegra.style.background = "black";
+	cajaNegra.style.width = "40px";
+	cajaNegra.style.height = "40px";
+
+	cajaAzul.appendChild(cajaNegra);
+	cajaAzul.appendChild(cajaVerde);
+	contenedor.appendChild(cajaAzul);
+}
+
 crearCajaRoja();
+crearCajaAzul();
